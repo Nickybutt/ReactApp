36a9ccee916b021e56f6199481e1364671ceca48
buffalowing <nick_verhagen757@hotmail.com>
2018-07-11 15:15:10 +0200

    Deleted extra files

--- react-workshop/src/App.js
+++ react-workshop/src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+const list = [
+  {
+  title: "react",
+  url: 'https://github.com/Nickybutt/ReactApp/pull/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+}, {
+  title: 'Redux',
+  url: 'https://github.com/reactjs/redux',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+}, ];
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+        Title: list.title
+
+        </div>
     );
   }
 }

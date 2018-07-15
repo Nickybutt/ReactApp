f188d7ff177aca1bd22a4053fbac8cfc29c616ef
buffalowing <nick_verhagen757@hotmail.com>
2018-07-15 11:10:20 +0200

    Made the components for the new site. also made the searchbar which can take a search term

--- react-workshop/src/App.js
+++ react-workshop/src/App.js
@@ -1,30 +1,40 @@
 import React, { Component } from 'react';
+import SearchBar from './components/search-bar';
 
-
-const list = [
-  {
-  title: "react",
-  url: 'https://github.com/Nickybutt/ReactApp/pull/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
-}, {
-  title: 'Redux',
-  url: 'https://github.com/reactjs/redux',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
-}, ];
+import Data from './data/data.js';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { newsFeed: []};
+
+    const list = [
+      {
+      title: "react",
+      url: 'https://github.com/Nickybutt/ReactApp/pull/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    }, {
+      title: 'Redux',
+      url: 'https://github.com/reactjs/redux',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }, ];
+
+    console.log(list);
+
+  }
   render() {
     return (
       <div>
-        Title: list.title
-
+        <SearchBar />
+        <button >dismiss</button>      
         </div>
     );
   }
